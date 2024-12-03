00c0a7cb98dca714ce1de4371b7b29eda14c8763
Matheo Marchioni <matheomarchioni@gmail.com>
2024-12-03 16:29:20 +0100

    ajout code

--- main.py
+++ main.py
@@ -20,16 +20,57 @@ liste_des_sprites = pygame.sprite.LayeredUpdates()
 liste_des_sprites.add(fond)
 
 
+class Fantome(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        self.image = pygame.image.load("fantome1.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+x_aleatoire = randint(20,40)
+y_aleatoire = randint(20,40)
+
+fantome1 = Fantome(x_aleatoire,y_aleatoire)
 
 
-perso = pygame.sprite.Sprite()
-pygame.sprite.Sprite.__init__(perso)
-perso.image = pygame.image.load("persojaune.png").convert()
-perso.rect = perso.image.get_rect()
-perso.rect.x = 30
-perso.rect.y = 30
+class Pacman(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("persojaune.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = 30
+        self.rect.y = 30
+        self.angle = 0
+        self.perso = 0
+
+
+    def tourne_vers_haut(self):
+        angle_de_rotation = 90 - self.angle
+        self.image = pygame.transform.rotate(self.image, angle_de_rotation)
+        self.angle = 90
+
+    def tourne_vers_gauche(self):
+        angle_de_rotation = 180 - self.angle
+        self.image = pygame.transform.rotate(self.image, angle_de_rotation)
+        self.angle = 180
+
+    def tourne_vers_bas(self):
+        angle_de_rotation = 270 - self.angle
+        self.image = pygame.transform.rotate(self.image, angle_de_rotation)
+        self.angle = 270
+
+    def tourne_vers_droit(self):
+        angle_de_rotation = 0 - self.angle
+        self.image = pygame.transform.rotate(self.image, angle_de_rotation)
+        self.angle = 0
+
+
+perso = Pacman()
+
 
 liste_des_sprites.add(perso)
+liste_des_sprites.add(fantome1)
 liste_des_sprites.add(fond)
 
 
@@ -48,6 +89,7 @@ while continuer:
         if event.type == KEYDOWN:
             if event.key == K_RIGHT:
                 direction = "droite"
+
             if event.key == K_DOWN:
                 direction = "bas"
             if event.key == K_LEFT:
@@ -55,18 +97,30 @@ while continuer:
             if event.key == K_UP:
                 direction = "haut"
 
+
     if direction == "droite":
         perso.rect.x += 3
+        perso.tourne_vers_droit()
+
     elif direction == "bas":
         perso.rect.y += 3
+        perso.tourne_vers_bas()
     elif direction == "gauche":
         perso.rect.x += -3
+        perso.tourne_vers_gauche()
     elif direction == "haut":
         perso.rect.y += -3
-
+        perso.tourne_vers_haut()
 
     pygame.display.flip()
     fenetre.fill((0, 0, 0))
     clock.tick(60)
 
+
+
+
 pygame.quit()
+
+
+
+
